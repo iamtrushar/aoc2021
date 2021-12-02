53bc564eb7a9fc6a58428d01058c6dc743eaf037
Trushar Mistry <tmistry@landworks.com>
2021-12-02 11:45:56 -0600

    Add day2

---
+++ src/day2/day2.ts
@@ -0,0 +1,55 @@
+import { inputReadNavigations, Navigation } from "../inFileReader";
+
+const inputByLine = inputReadNavigations("./src/day2/input.txt");
+console.log(`Part 1, answer: ${part1(inputByLine)}`);
+console.log(`Part 2, answer: ${part2(inputByLine)}`);
+
+
+function part1(report: Array<Navigation>): number {
+
+    let forward = 0;
+    let depth = 0;
+    let vertical = "";
+
+    report.forEach(element => {
+
+        if (element.direction === "F") {
+
+            forward = forward + element.move;
+        }
+        else {
+
+            if (vertical === "") { vertical = element.direction; }
+            depth = (element.direction === vertical)
+                ? depth + element.move
+                : depth - element.move;
+        }
+    });
+    return (forward * depth);
+}
+
+
+function part2(report: Array<Navigation>): number {
+
+    let forward = 0;
+    let depth = 0;
+    let aim = 0;
+    let vertical = "";
+
+    report.forEach(element => {
+
+        if (element.direction === "F") {
+
+            forward = forward + element.move;
+            if (aim !== 0) { depth = depth + (element.move * aim); }
+        }
+        else {
+
+            if (vertical === "") { vertical = element.direction; }
+            aim = element.direction === vertical && element.direction == "D"
+                ? aim + element.move
+                : aim = aim - element.move;
+        }
+    });
+    return (forward * depth);
+}
