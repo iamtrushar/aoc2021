82d250196db5d2dfd4ac15bc6c989119eaf7acf8
Trushar Mistry <tmistry@landworks.com>
2021-12-02 12:05:48 -0600

    Clean up function exports

--- src/inFileReader.ts
+++ src/inFileReader.ts
@@ -1,38 +1,34 @@
 import * as fs from "fs";
 
-export function inputReadNumbers(path: string): number[] {
-    const input: string = fs.readFileSync(path, "utf8");
-    return input.split("\n").map(Number);
-}
+const readFile = (path: string) => fs.readFileSync(path, "utf8").split("\n");
+
+export const inputReadNumbers = (path: string): number[] => readFile(path).map(Number);
 
 export class Navigation {
     direction: string;
     move: number;
 }
 
-export function inputReadNavigations(path: string): Array<Navigation> {
-    const input: string = fs.readFileSync(path, "utf8");
-    const read =  input.split("\n").map(String);
+export const inputReadNavigations = (path: string): Array<Navigation> => {
 
+    const read = readFile(path).map(String);
     let nav = new Array<Navigation>();
-
     read.forEach(element => {
 
-        var n =new Navigation();
-        if(element.startsWith("forward")){
+        var n = new Navigation();
+        if (element.startsWith("forward")) {
             n.direction = "F";
             n.move = parseInt(element.substring("forward".length).trim());
         }
-        else if(element.startsWith("down")){
+        else if (element.startsWith("down")) {
             n.direction = "D";
             n.move = parseInt(element.substring("down".length).trim());
         }
-        else if(element.startsWith("up")){
+        else if (element.startsWith("up")) {
             n.direction = "U";
             n.move = parseInt(element.substring("up".length).trim());
         }
         nav.push(n);
     });
-
     return nav;
 }
