c9ebc5e528c929cd04869950cfeb89cc608886a6
Trushar Mistry <tmistry@landworks.com>
2021-12-20 07:25:38 -0600

    Add day13 part 2

--- src/day13/day13.ts
+++ src/day13/day13.ts
@@ -8,10 +8,9 @@ inputByLine.forEach(row => {
     points.push(row.indexOf('\r') !== -1 ? row.substring(0, row.length - 1) : row)
 });
 
-console.log(`Part 1, answer : ${part1()}`);
-//console.log(`Part 2, answer : ${part2()}`);
+console.log(part());
 
-function part1(): number {
+function part(): void {
 
     let locations = new Array<GridLocation>();
     let instructions = new Array<GridLocation>();
@@ -37,35 +36,40 @@ function part1(): number {
         }
     }
 
-    const xMax = Math.max(...locations.map(l => l.x));
-    const yMax = Math.max(...locations.map(l => l.y));
-
-    // for (let i = 0; i <= xMax; i++) {
-    //     for (let j = 0; j <= yMax; j++) {
-    //         if (locations.findIndex(l => l.x === i && l.y === j) === -1) {
-    //             locations.push(new GridLocation(i, j, "."));
-    //         }
-    //     }
-    // }
-
-
-    console.log(`Previous: ${locations.filter(l => l.reading === "#").length}`);
+    // fold instructions
     for (let i = 0; i < instructions.length; i++) {
-
         let element = instructions[i];
         if (element.y !== -1) {
             locations = foldUp(element.y, locations);
         }
         else {
             locations = foldLeft(element.x, locations);
-
         }
 
-        console.log(locations.filter(l => l.reading === "#").length);
-        break;
+        if (i == 0) {
+            console.log(`Part 1, answer : ${locations.filter(l => l.reading === "#").length}`);
+        }
     }
 
-    return -1;
+    const xMax = Math.max(...locations.map(l => l.x));
+    const yMax = Math.max(...locations.map(l => l.y));
+
+    let print = Array(xMax + 1).fill(".").map(x => Array(yMax + 1).fill("."))
+
+    for (let i = 0; i <= xMax; i++) {
+        let printer = "";
+        for (let j = 0; j <= yMax; j++) {
+
+            if (locations.filter(l => l.x === i && l.y === j).length > 0) {
+               printer = printer + "#";
+               print[i][j] = "#";
+            }
+            printer = printer + " ";
+        }
+        console.log(printer);
+    }
+
+    console.log(`Part 2, answer (visualize in debugger instead) : ${print}`);
 }
 
 
@@ -82,18 +86,13 @@ function foldUp(y: number, locations: Array<GridLocation>): Array<GridLocation> 
     });
 
     let gls = new Array<GridLocation>();
-    //const gl = locations.filter(l => l.x < y);
-    //gl.forEach(ele => { gls.push(new GridLocation(ele.x, ele.y, ele.reading)) });
-
-    firstHalf.forEach(ele => { gls.push(new GridLocation(ele.x, ele.y, ele.reading)) });
+    firstHalf.forEach(ele => {
+        gls.push(new GridLocation(ele.x, ele.y, ele.reading))
+    });
     secondHalf.forEach(ele => {
-
 
         if (gls.filter(p => p.x === ele.x && p.y === ele.y).length == 0) {
             gls.push(new GridLocation(ele.x, ele.y, ele.reading));
-        }
-        else {
-            //console.log(`x:${ele.x},y${ele.y}`)
         }
     });
 
@@ -113,38 +112,13 @@ function foldLeft(x: number, locations: Array<GridLocation>): Array<GridLocation
     });
 
     let gls = new Array<GridLocation>();
-    // const gl = locations.filter(l => l.y < x);
-    // gl.forEach(ele => { gls.push(new GridLocation(ele.x, ele.y, ele.reading)) });
-
-    leftHalf.forEach(ele => { gls.push(new GridLocation(ele.x, ele.y, ele.reading)) });
+    leftHalf.forEach(ele => { 
+        gls.push(new GridLocation(ele.x, ele.y, ele.reading));
+    });
     rightHalf.forEach(ele => {
-
         if (gls.filter(p => p.x === ele.x && p.y === ele.y).length == 0) {
-            gls.push(new GridLocation(ele.x, ele.y, ele.reading)) ;
-        }
-        else {
-            //console.log(`x:${ele.x},y${ele.y}`)
+            gls.push(new GridLocation(ele.x, ele.y, ele.reading));
         }
     });
     return gls;
 }
-
-// function checkLeft(current: Location, all: Array<Location>): Location {
-
-//     return all.filter(p => p.x === current.x && (p.y === current.y - 1))[0]
-// }
-
-// function checkRight(current: Location, all: Array<Location>): Location {
-
-//     return all.filter(p => p.x === current.x && p.y === (current.y + 1))[0]
-// }
-
-// function checkTop(current: Location, all: Array<Location>): Location {
-
-//     return all.filter(p => p.x === (current.x - 1) && p.y === current.y)[0]
-// }
-
-// function checkBottom(current: Location, all: Array<Location>): Location {
-
-//     return all.filter(p => p.x === (current.x + 1) && p.y === current.y)[0]
-// }
